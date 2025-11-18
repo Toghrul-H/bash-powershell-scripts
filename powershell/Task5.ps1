5464cc9a2729f9a4b6aeee114a0abbda420b2de2
Toghrul Hasanli <toghrulhasanli2@gmail.com>
2025-11-18 19:48:30 +0100

    Add files via upload

---
+++ powershell/Task5.ps1
@@ -0,0 +1,8 @@
+﻿$a = $args[0]
+$b = $args[1]
+$c = $args[2]
+$discriminant = [Math]::Sqrt(($b * $b) - (4 * $a * $c))
+$root1 = (-$b + $discriminant) / (2 * $a)
+$root2 = (-$b - $discriminant) / (2 * $a)
+Write-Host "Root 1: $root1"
+Write-Host "Root 2: $root2"
